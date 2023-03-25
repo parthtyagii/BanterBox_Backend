669b9f6b900d98f8fcab0b3ecd04800ea755f966
Parth Tyagi <parthtyagicode@gmail.com>
2023-03-25 21:08:09 +0530

    authentication routes setup

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const cors = require('cors');
+
+//--------------------------------------------------------------------------------
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+//--------------------------------------------------------------------------------
+
+app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 
@@ -12,6 +32,6 @@ const mongoose = require('mongoose');
 
 
 
-app.listen(1000,()=>{
+app.listen(1000, () => {
     console.log('Server 1000 running!');
 })
--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userModel = mongoose.Schema({
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
@@ -15,10 +17,22 @@ const userModel = mongoose.Schema({
     },
     pic: {
         type: String,
-        required: true,
         default: 'https://res.cloudinary.com/dw0up71e2/image/upload/v1675568191/profileImages/yaymwg5brzhvc5ww1pnb.webp'
     },
 }, { timestamps: true });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return (bcrypt.compareSync(enteredPassword, this.password));
+};
+
+userSchema.pre('save', async function (next) {
+    if (!this.modified) {
+        next();
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    this.password = bcrypt.hashSync(this.password, salt);
+});
 
 
 const User = mongoose.model('User', userSchema);
