8023c8bae7a1283c1beced742c7665ca6133b5c5
Parth Tyagi <parthtyagicode@gmail.com>
2023-04-02 15:45:57 +0530

    Message APIs added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const cors = require('cors');
 
@@ -21,6 +22,7 @@ app.use(cors());
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
