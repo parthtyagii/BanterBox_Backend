ffaea234ad10022b9da676ffeaf897d70da5564f
Parth Tyagi <parthtyagicode@gmail.com>
2023-04-04 19:15:27 +0530

    added script

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'https://banterbox.netlify.app/',
+        origin: 'https://banterbox.netlify.app',
     }
 });
 
